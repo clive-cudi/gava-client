d72c70eac4490ea19525d2c9a342a91d597047f1
clive-cudi <clivemaina41@gmail.com>
2023-07-27 11:00:34 +0300

    project-template-init

---
+++ src/menu-items/pages.js
@@ -0,0 +1,36 @@
+// assets
+import { IconKey } from '@tabler/icons';
+
+// constant
+const icons = {
+  IconKey
+};
+
+// ==============================|| EXTRA PAGES MENU ITEMS ||============================== //
+
+const pages = {
+  id: 'pages',
+  title: 'Pages',
+  caption: 'Pages Caption',
+  type: 'group',
+  children: [
+    {
+      id: 'authentication',
+      title: 'Authentication',
+      type: 'collapse',
+      icon: icons.IconKey,
+
+      children: [
+        {
+          id: 'logout',
+          title: 'Logout',
+          type: 'item',
+          url: '/pages/login/login3',
+          target: true
+        }
+      ]
+    }
+  ]
+};
+
+export default pages;
